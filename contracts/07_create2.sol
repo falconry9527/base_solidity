7cb9ff68c56a47e2385184a26de848a9236014af
conry <kequan888@gmail.com>
2025-10-15 16:09:45 +0800

    CREATE2

---
+++ contracts/07_create2.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./DeployMe.sol";
+
+contract Factory {
+    event Deployed(address addr, uint256 salt);
+
+    // 使用 CREATE2 部署子合约
+    function deploy(uint256 _value, uint256 _salt) public returns (address) {
+        bytes memory bytecode = type(DeployMe).creationCode;
+        bytecode = abi.encodePacked(bytecode, abi.encode(_value));
+
+        address addr;
+        assembly {
+            addr := create2(0, add(bytecode, 0x20), mload(bytecode), _salt)
+            if iszero(extcodesize(addr)) { revert(0, 0) }
+        }
+
+        emit Deployed(addr, _salt);
+        return addr;
+    }
+
+    // 部署前预测地址
+    function getAddress(uint256 _value, uint256 _salt) public view returns (address) {
+        bytes memory bytecode = type(DeployMe).creationCode;
+        bytecode = abi.encodePacked(bytecode, abi.encode(_value));
+
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(this),
+                _salt,
+                keccak256(bytecode)
+            )
+        );
+        return address(uint160(uint256(hash)));
+    }
+}
